c59d6e7e86262857e479f4a9ad0275ac275a5466
Junichi Kato <j5ik2o@gmail.com>
2013-04-22 02:13:19 +0900

    - ライセンスファイルの配置 - ヘッダー追加

--- project/Build.scala
+++ project/Build.scala
@@ -10,7 +10,10 @@ object ScalatoolboxBuild extends Build {
       name := "scala-toolbox",
       organization := "org.sisioh.scala.toolbox",
       version := "0.1-SNAPSHOT",
-      scalaVersion := "2.10.1"
+      scalaVersion := "2.10.1",
+      libraryDependencies ++= Seq(
+        "org.clapper" %% "grizzled-slf4j" % "1.0.1"
+      )
       // add other settings here
     )
   )
--- src/main/scala/org/sisioh/scala/toolbox/Scalatoolbox.scala
+++
@@ -1,5 +0,0 @@
-package org.sisioh.scala.toolbox
-
-object Scalatoolbox extends App {
-  println("Hello, scala-toolbox")
-}
