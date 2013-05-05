034933263e908716ddced7e4ea9c16a5b625d0a9
Junichi Kato <j5ik2o@gmail.com>
2013-05-05 21:05:31 +0900

    specsを修正

--- src/test/scala/org/sisioh/scala/toolbox/OptionOpsSpec.scala
+++ src/test/scala/org/sisioh/scala/toolbox/OptionOpsSpec.scala
@@ -11,11 +11,15 @@ class OptionOpsSpec extends Specification {
       val intSomeOp: Option[Int] = Some(1)
       intSomeOp.toTry.isSuccess must beTrue
       intSomeOp.toTry.get must_== 1
+      intSomeOp.toTry.toOption must_== intSomeOp
 
       val intNoneOp: Option[Int] = None
       intNoneOp.toTry.isFailure must beTrue
       intNoneOp.toTry.failed.get must haveClass[NoSuchElementException]
-      intNoneOp.toTry(new Exception).failed.get must haveClass[Exception]
+      intNoneOp.toTry.toOption must_== intNoneOp
+
+      val ex = new Exception
+      intNoneOp.toTry(ex).failed.get must_== ex
     }
   }
 
