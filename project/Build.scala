de7fea4df8dbcf6a66573a9466dd52b2faa91788
Junichi Kato <j5ik2o@gmail.com>
2015-05-16 17:02:57 +0900

    Added support for sbt-release & sbt-sonatype.

--- project/Build.scala
+++ project/Build.scala
@@ -1,3 +1,4 @@
+import xerial.sbt.Sonatype.SonatypeKeys._
 import sbt._
 import sbt.Keys._
 
@@ -8,12 +9,15 @@ object ScalatoolboxBuild extends Build {
     base = file("."),
     settings = Project.defaultSettings ++ Seq(
       name := "scala-toolbox",
+      sonatypeProfileName := "org.sisioh",
       organization := "org.sisioh",
-      version := "0.0.9-SNAPSHOT",
-      scalaVersion := "2.11.0",
-      crossScalaVersions := Seq("2.11.0", "2.10.4"),
+      scalaVersion := "2.11.6",
+      crossScalaVersions := Seq("2.11.6", "2.10.5"),
+      resolvers ++= Seq(
+        "Scalaz Bintray Repo" at "http://dl.bintray.com/scalaz/releases"
+      ),
       libraryDependencies ++= Seq(
-        "org.specs2" %% "specs2" % "2.3.12" % "test",
+        "org.specs2" %% "specs2-core" % "3.6" % "test",
         "org.mockito" % "mockito-all" % "1.9.0" % "test",
         "ch.qos.logback" % "logback-classic" % "1.0.7" % "test",
         "org.slf4j" % "slf4j-api" % "1.7.1",
@@ -24,14 +28,6 @@ object ScalatoolboxBuild extends Build {
       publishArtifact in Test := false,
       pomIncludeRepository := {
         _ => false
-      },
-      publishTo <<= version {
-        (v: String) =>
-          val nexus = "https://oss.sonatype.org/"
-          if (v.trim.endsWith("SNAPSHOT"))
-            Some("snapshots" at nexus + "content/repositories/snapshots")
-          else
-            Some("releases" at nexus + "service/local/staging/deploy/maven2")
       },
       pomExtra := (
         <url>https://github.com/sisioh/sisioh-toolbox</url>
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,3 +1,8 @@
-addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.5.2")
+addSbtPlugin("org.xerial.sbt" % "sbt-sonatype" % "0.5.0")
 
-addSbtPlugin("com.typesafe.sbt" % "sbt-pgp" % "0.8")
+addSbtPlugin("com.jsuereth" % "sbt-pgp" % "1.0.0")
+
+addSbtPlugin("com.typesafe.sbt" % "sbt-scalariform" % "1.3.0")
+
+addSbtPlugin("com.github.gseitz" % "sbt-release" % "1.0.0")
+
